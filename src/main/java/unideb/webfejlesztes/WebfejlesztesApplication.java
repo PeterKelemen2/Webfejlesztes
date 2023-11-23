62e95147095d4bb18fb3518e22392440994f831c
Kelemen Péter <peter.kelemen456@gmail.com>
2023-11-23 01:25:41 +0100

    Added base login page

--- src/main/java/unideb/webfejlesztes/WebfejlesztesApplication.java
+++ src/main/java/unideb/webfejlesztes/WebfejlesztesApplication.java
@@ -6,8 +6,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class WebfejlesztesApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(WebfejlesztesApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(WebfejlesztesApplication.class, args);
+    }
+
 
 }
